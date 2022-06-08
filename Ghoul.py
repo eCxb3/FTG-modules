4ab4f453c70d1d23ba0b63718244d2764c5e0a08
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-06-08 22:18:37 +0300

    Update and rename Ghoul to Ghoul.py

---
+++ Ghoul.py
@@ -0,0 +1,42 @@
+from asyncio import sleep
+from .. import loader, utils
+from pyrogram.types import Message
+
+@loader.tds
+class GhoulMod(loader.module):
+	
+	"""Я гуль"""
+	
+	async def ghoulom_cmd(self, message: Message):
+		await message.edit('Я гуль')
+		await sleep(2)
+		
+		ghoul = 1000
+		while ghoul > 0:
+			await message.edit(f'{ghoul} - 7 = {ghoul - 7}')
+			ghoul -= 7
+			await sleep(0.1)
+		return await message.edit(f'l l let me die')
+	
+	async def ghoulmm_cmd(self, message: Message):
+		await message.edit('Я гуль')
+		await sleep(2)
+		
+		ghoul = 1000
+		while ghoul > 0:
+			await client.send_message(message.chat.id, f''{ghoul} - 7 = {ghoul - 7})
+			ghoul -= 7
+			await sleep(0.1)
+		return await client.send_message(message.chat.id, f'l l let me die')
+	
+	async def ghoulmmd_cmd(self, message: Message):
+		await message.edit('Я гуль')
+		await sleep(2)
+		
+		ghoul = 1000
+		while ghoul > 0:
+			d = await client.send_message(message.chat.id, f'{ghoul} - 7 = {ghoul - 7}')
+			ghoul -= 7
+			await sleep(0.4)
+			await d.delete()
+		return await client.send_message(message.chat.id, 'l l let me die')
