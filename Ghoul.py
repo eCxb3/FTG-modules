d154fb810ee6bdfdf364c2cc64860594c48719ca
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-06-08 23:34:01 +0300

    Update Ghoul.py

--- Ghoul.py
+++ Ghoul.py
@@ -3,10 +3,8 @@ from .. import loader, utils
 from pyrogram.types import Message
 
 @loader.tds
-class GhoulMod(loader.Module):
-	
+class GhoulMod(loader.Module)
 	"""Я гуль"""
-	
 	strings = {
         	"name": "Ghoul"
 	}
@@ -15,10 +13,8 @@ class GhoulMod(loader.Module):
         	self._db = db
         	self._client = client
 	
-	async def ghoulomcmd(self, message: Message):
-		
+	async def ghoulomcmd(self, message: Message):	
 		"""Гуль одним сообщением"""
-		
 		await utils.answer(message, 'Я гуль')
 		await sleep(2)
 		
@@ -30,30 +26,26 @@ class GhoulMod(loader.Module):
 		return await utils.answer(message, f'l l let me die')
 	
 	async def ghoulmmcmd(self, message: Message):
-		
-		"""Гуль множеством сообщений"""
-		
+		"""Гуль множеством сообщений"""	
 		await utils.answer(message, 'Я гуль')
 		await sleep(2)
 		
 		ghoul = 1000
 		while ghoul > 0:
-			await client.send_message(message.chat.id, f'{ghoul} - 7 = {ghoul - 7}')
+			await message.reply(f'{ghoul} - 7 = {ghoul - 7}')
 			ghoul -= 7
 			await sleep(0.1)
-		return await client.send_message(message.chat.id, f'l l let me die')
+		return await message.reply(f'l l let me die')
 	
 	async def ghoulmmdcmd(self, message: Message):
-		
-		"""Гуль множеством сразу удаляемыемых сообщений"""
-		
+		"""Гуль множеством сразу удаляемыемых сообщений"""	
 		await utils.answer(message, 'Я гуль')
 		await sleep(2)
 		
 		ghoul = 1000
 		while ghoul > 0:
-			d = await client.send_message(message.chat.id, f'{ghoul} - 7 = {ghoul - 7}')
+			d = await message.reply(f'{ghoul} - 7 = {ghoul - 7}')
 			ghoul -= 7
 			await sleep(0.4)
 			await d.delete()
-		return await client.send_message(message.chat.id, f'l l let me die')
+		return await message.reply(f'l l let me die')
