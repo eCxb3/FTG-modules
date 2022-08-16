cb4f4615df4c18bedda395297a56a29bc4eac00e
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-08-16 17:15:38 +0300

    Update MSendAuto.py

--- MSendAuto.py
+++ MSendAuto.py
@@ -2,22 +2,22 @@ from .. import loader, utils
 from telethon.tl.types import Message
 
 @loader.tds
-class MusecsendMod(loader.Module):
+class MusicsendMod(loader.Module):
   """Отправляет песню в группу"""
   strings = {"name": "MSend"}
   
   async def client_ready(self, client, db):
     self._db = db
     self._client = client
-    
-    
+
   async def watcher(self, message: Message):
     if message.from_id == 5552353032:
       assert message.document
+      reply = await message.get_reply_message()
       await self._client.send_file(
         1632497562,
         message,
-        caption=f'<a href="{reply.entities[0].url}">song.link</a>',
+        caption=f'<a href="{reply.entities[0].url}">song.link</a> | <a href="{reply.entities[3].url}">via</a>',
         parse_mode='HTML',
         reply_to=getattr(message, "reply_to_msg_id", None),
       )
