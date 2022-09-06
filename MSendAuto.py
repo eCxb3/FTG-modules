9de76c7d6675d11bc459fd14dee8c67ae078c84a
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-09-06 17:53:51 +0300

    Update MSendAuto.py

--- MSendAuto.py
+++ MSendAuto.py
@@ -11,13 +11,16 @@ class MusicsendMod(loader.Module):
     self._client = client
 
   async def watcher(self, message: Message):
-    assert message.from_id
-    if message.from_id == 5552353032:
-      assert message.document
-      await self._client.send_file(
-        1632497562,
-        message,
-        caption=f'<a href="{message.entities[0].url}">song.link</a> | <a href="{message.entities[3].url}">via</a>',
-        parse_mode='HTML',
-        reply_to=getattr(message, "reply_to_msg_id", None),
-      )
+    try:
+      assert message.from_id
+      if message.from_id == 1792795014:
+        assert message.document
+        await self._client.send_file(
+          1632497562,
+          message,
+          caption=f'<a href="{message.entities[0].url}">song.link</a> | <a href="{message.entities[3].url}">via</a>',
+          parse_mode='HTML',
+          reply_to=getattr(message, "reply_to_msg_id", None),
+        )
+    except:
+      pass
